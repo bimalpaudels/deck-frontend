2bad9e6148de38ad2567c185c115c437d5a2b35d
Bimal Paudel <ibimalp@gmail.com>
2024-03-22 15:40:44 +0100

    Header and Footer update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-4 text-center fixed bottom-0 w-full">
-      <div className="container mx-auto">
-        <p>&copy; {new Date().getFullYear()} BMlDeck. All rights reserved.</p>
+    // <footer className="bg-gray-950 text-white py-4 text-center fixed bottom-0 w-full">
+    //   <div className="container mx-auto">
+    //     <p>&copy; {new Date().getFullYear()} BMlDeck. All rights reserved.</p>
+    //   </div>
+    // </footer>
+    <footer className="py-4 bg-gray-950 fixed bottom-0 w-full">
+      <div className="flex flex-col items-center text-sm justify-between gap-4 text-slate-300">
+        <p>
+          &copy; {new Date().getFullYear()} BMlDeck. All rights reserved. Code
+          available on{" "}
+          <Link href="https://github.com/bimalpaudels" className="underline">
+            GitHub
+          </Link>
+        </p>
       </div>
     </footer>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -1,29 +1,33 @@
+import Link from "next/link";
+import {
+  CardStackMinusIcon,
+  ReaderIcon,
+  PersonIcon,
+  QuestionMarkCircledIcon,
+} from "@radix-ui/react-icons";
+
 export default function Header() {
   return (
-    <header className="bg-gray-900 text-white py-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center">
-          {/* <img className="h-4 mr-4" src={"/next.svg"} alt="Logo" /> */}
-          <h1 className="text-lg font-bold">BMlDeck</h1>
-        </div>
-        <nav>
-          <ul className="flex">
-            <li className="mr-6">
-              <a href="#" className="hover:text-gray-300">
-                Home
-              </a>
-            </li>
-            <li className="mr-6">
-              <a href="#" className="hover:text-gray-300">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Contact
-              </a>
-            </li>
-          </ul>
+    <header className="sticky top-0 z-50 w-full bg-gray-950">
+      <div className="container h-14 flex items-center max-w-screen-2xl justify-between">
+        <Link
+          href="/deck"
+          className="flex text-slate-300 font-bold mr-6 space-x-2 items-center gap-2 text-sm"
+        >
+          <CardStackMinusIcon width="20" height="20" />
+          BMlDeck
+        </Link>
+
+        <nav className="flex items-center gap-6 text-sm text-slate-300 justify-between">
+          <Link href="/about" className="hover:text-gray-300">
+            <ReaderIcon width="20" height="20" />
+          </Link>
+          <Link href="/help" className="hover:text-gray-300">
+            <QuestionMarkCircledIcon width="20" height="20" />
+          </Link>
+          <Link href="/profile" className="hover:text-gray-300">
+            <PersonIcon width="20" height="20" />
+          </Link>
         </nav>
       </div>
     </header>
